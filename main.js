fdf4aba61f373f702214e55a82aa328f26806af2
Natalia Reggiani <nreggiani77@gmail.com>
2022-01-17 20:12:45 -0300

    cambio en alert

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
 const al = document.getElementById('alerta');
 al.addEventListener('click', function(){
-   alert(`ATENCION!!,\nContenido no apto para menores de 18 años`)        
+   alert(` Contenido no apto para menores de 18 años. Incluye chanchadas`)        
 })
