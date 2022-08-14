72e97ba9dbaab9ddb8b65afb2df9e0aa26c67049
Natalia Reggiani <nreggiani77@gmail.com>
2022-08-14 16:07:22 -0300

    catalogo

--- main.js
+++ main.js
@@ -25,5 +25,5 @@ cardpadre.forEach(el=>{
 
 let al = document.getElementById('alerta');
 al.addEventListener('click',function(){  
-  alert("Contenido no apto para menores de 18 años. Incluye chanchadas")    
+    alert("Contenido no apto para menores de 18 años. Incluye chanchadas")    
 })
