d2a0a6363d3e8755815daa3d6e1a9aca80f381e8
Natalia Reggiani <nreggiani77@gmail.com>
2022-11-02 08:06:22 -0300

    ajuste cards astronautas

--- main.js
+++ main.js
@@ -4,15 +4,15 @@ let cardpadre = document.querySelectorAll('.card-padre')
 let cardhover = document.querySelectorAll('.card-hover')
 console.log(cardpadre)
 function agregarDiv(){
-    this.childNodes[5].classList.remove('oculto')
+    this.childNodes[5].classList.remove('hide')
     this.childNodes[5].classList.remove('slide-out-top')
     this.childNodes[5].classList.add('slide-in-top')
+    
 }
 
 function sacarDiv(){
-    this.childNodes[5].classList.remove('slide-in-top')
     this.childNodes[5].classList.add('slide-out-top')
-    this.childNodes[5].classList.add('oculto')
+    this.childNodes[5].classList.remove('slide-in-top')
 }
 
 cardpadre.forEach(el=>{
